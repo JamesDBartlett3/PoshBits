0370c361236c9088ed01d7fef1bdd369ba7e849f
James D. Bartlett III <37491308+JamesDBartlett3@users.noreply.github.com>
2022-10-07 20:31:30 -0500

    Add files via upload

---
+++ Install_PowerShell_Modules.ps1
@@ -0,0 +1,85 @@
+# Install PowerShell Modules Useful for BI, DA, and DS Development
+
+if ($PSVersionTable.PSVersion.Major -lt 7) {
+	Write-Output "Please run this script with PowerShell Core version 7.0 or later."
+	Write-Output "Press any key to exit..."
+	$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+	exit
+} else {
+
+	# $isAdmin = (
+	# 	[Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
+	# 	).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+
+	# if (!$isAdmin){
+	# 	$a = "-NoProfile -NonInteractive -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+	# 	Start-Process pwsh.exe $a -Verb RunAs; exit}
+
+	# Declare list of PowerShell modules to install
+	[PSCustomObject]$modules = @(
+		"Az.Accounts"
+		, "Az.AnalysisServices"
+		, "Az.ApiManagement"
+		, "Az.AppConfiguration"
+		, "Az.DataFactory"
+		, "Az.DataLakeAnalytics"
+		, "Az.DataLakeStore"
+		, "Az.Functions"
+		, "Az.LogAnalytics"
+		, "Az.PowerBIEmbedded"
+		, "Az.Resources"
+		, "Az.Sql"
+		, "Az.Storage"
+		, "Az.Synapse"
+		, "AzureAD"
+		, "AzureADPreview"
+		, "Configuration"
+		, "DataGateway"
+		, "DataGateway.Profile"
+		, "DataMashup"
+		, "dbatools"
+		, "dbops"
+		, "ExchangePowerShell"
+		, "ImportExcel"
+		, "InvokeBuild"
+		, "Metadata"
+		, "MicrosoftPowerBIMgmt"
+		, "MicrosoftTeams"
+		, "Microsoft.Online.SharePoint.PowerShell"
+		, "Microsoft.Graph"
+		, "ModuleBuilder"
+		, "MSOnline"
+		, "oh-my-posh"
+		, "OnPremisesDataGatewayHAMgmt"
+		, "Pester"
+		, "posh-git"
+		, "PowerHTML"
+		, "PowerShell-Beautifier"
+		, "PowerShellForGitHub"
+		, "PowerShellGet"
+		, "PowerShellNotebook"
+		, "PowerShellProTools"
+		, "ps2exe"
+		, "PSFramework"
+		, "PSKoans"
+		, "PSReadLine"
+		, "PSRequiredModules"
+		, "PSScriptAnalyzer"
+		, "PSScriptTools"
+		, "ReportingServicesTools"
+		, "SqlServer"
+	)
+
+	# Loop through $modules object and install each module
+	foreach ($module in $modules) {
+		Write-Output "-----------------------------------------------------------------------------------------------"
+		Write-Output "Installing module: '$module'..."
+		Install-Module -Name $module -Scope CurrentUser -Repository PSGallery -AllowPrerelease
+	}
+
+	Write-Output "-----------------------------------------------------------------------------------------------"
+
+	# Update local help cache
+	Update-Help
+
+}
