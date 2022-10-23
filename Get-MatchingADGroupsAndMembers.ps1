eedbe204d2a5a1d6077870f87fba5e85bd7f3aaa
James D. Bartlett III <james.bartlett@gmail.com>
2022-10-23 18:27:30 -0500

    updated ToDo section

--- Get-MatchingADGroupsAndMembers.ps1
+++ Get-MatchingADGroupsAndMembers.ps1
@@ -1,13 +1,12 @@
-# TODO: Parameterize SearchTerm, GroupCategory
-# TODO: Add parameter to choose AD, AAD, or both
-# TODO: #Requires section
-
+# TODO: Parameterize GroupCategory
+# TODO: Parameter to choose AD, AAD, or both
 
 Function Get-MatchingADGroupsAndMembers {
   Param(
     [Parameter(Mandatory=$true)]
     [string]$SearchTerm
   )
+
   $groups = Get-ADGroup -Filter "GroupCategory -eq 'Security' -and Name -like '*$searchTerm*'"
   ForEach($g in $groups) {
     $gName = $g.Name
