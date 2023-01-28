089bf72ac45b574f4221674f73e00ea6cc27000c
James Bartlett <jbartlett@dmu.edu>
2023-01-27 19:27:22 -0600

    added VideoCodec param

--- Compress-WebinarVideo.ps1
+++ Compress-WebinarVideo.ps1
@@ -12,23 +12,44 @@ function Compress-WebinarVideo {
 
     Param(
         [Parameter(Mandatory=$True, Position=0, ValueFromPipeline=$True)]
-            [string]$InputFile,
-        [Parameter(Mandatory=$False, Position=1, ValueFromPipeline=$False)]
-            [int]$FrameRate=5,
-        [Parameter(Mandatory=$False, Position=2, ValueFromPipeline=$False)]
-            [switch]$UseNvidiaGPU
+            [string]$InputFile
+        ,[Parameter(Mandatory=$False, Position=1, ValueFromPipeline=$False)]
+            [int]$FrameRate=10
+        ,[Parameter(Mandatory=$False, Position=2, ValueFromPipeline=$False)]
+            [string]$VideoCodec
+        # ,[Parameter(Mandatory=$False, ValueFromPipeline=$False)]
+        #     [switch]$UseNvidiaGPU
+        # ,[Parameter(Mandatory=$False, ValueFromPipeline=$False)]
+        #     [switch]$UseAMDGPU
     )
 
-    [string]$targetDir = Get-ItemPropertyValue -LiteralPath $InputFile -Name FullName | Split-Path -Path {$_}
+    [string]$inputFileFullName = Get-ItemPropertyValue -LiteralPath $InputFile -Name FullName
+    # [string]$targetDir = $inputFileFullName | Split-Path -Path {$_}
     [string]$inputFileBase = Get-ItemPropertyValue -LiteralPath $InputFile -Name BaseName
     [string]$inputFileExtension = Get-ItemPropertyValue -LiteralPath $InputFile -Name Extension
-    [string]$outputFile = "$(Join-Path -Path $targetDir -ChildPath $inputFileBase)_[" + $FrameRate + "fps]" + $inputFileExtension
+    [string]$tempFile = "$($inputFileFullName)_temp_$($inputFileExtension)"
+    [string]$inputFileNewName = "$($inputFileBase)_original$($inputFileExtension)"
+    [string]$outputFileName = $inputFileBase + $inputFileExtension
+    [string]$outputVideoCodec = $VideoCodec ? "libx$($VideoCodec)" : "copy"
 
-    if ($UseNvidiaGPU) {
-        ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i $InputFile -vf fps=$FrameRate -c:v hevc_nvenc -ac 1 -ar 22050 $outputFile
-    } else {
-        #ffmpeg -i $InputFile -c:v libx265 -filter:v fps=fps=$FrameRate -ac 1 -ar 22050 $outputFile        
-        ffmpeg -i $InputFile -vf fps=$FrameRate -c:v libx265 -ac 1 -ar 22050 $outputFile
-    }
+    # if ($UseNvidiaGPU) {
+    #     ffmpeg `
+    #         -hwaccel cuda -hwaccel_output_format cuda `
+    #         -i $InputFile `
+    #         -c:a $outputAudioCodec -c:v $outputVideoCodec `
+    #         -vf fps=$FrameRate `
+    #         -ac 1 -ar 22050 `
+    #         $outputFile
+    # } else {     
+        ffmpeg `
+            -hwaccel auto `
+            -i $InputFile `
+            -vf fps=$FrameRate `
+            -c:v $outputVideoCodec `
+            -ac 1 -ar 22050 `
+            $tempFile
+    # }
+    Rename-Item -LiteralPath $inputFileFullName -NewName $inputFileNewName
+    Rename-Item -LiteralPath $tempFile -NewName $outputFileName
 
 }
