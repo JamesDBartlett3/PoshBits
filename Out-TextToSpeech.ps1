48bff92fcf4a8bf1e2a72001c88aed7761c5e776
James Bartlett <jbartlett@dmu.edu>
2023-01-31 16:24:07 -0600

    added list of voices; shortened voice to name only

--- Out-TextToSpeech.ps1
+++ Out-TextToSpeech.ps1
@@ -1,13 +1,27 @@
 
 # A function which takes a string as input and converts it to speech
 # Requires the System.Speech assembly
+# Possible voices are: 
+#   - Microsoft David Desktop
+#   - Microsoft Zira Desktop
+#   - Microsoft Hazel Desktop
+#   - Microsoft Mark Desktop
+#   - Microsoft Mike Desktop
+#   - Microsoft Mary Desktop
+#   - Microsoft Sam Desktop
+#   - Microsoft Anna Desktop
+#   - Microsoft Elsa Desktop
+#   - Microsoft Laura Desktop
+#   - Microsoft Benjamin Desktop
+#   - Microsoft Hedda Desktop
+
 
 function Out-TextToSpeech {
     Param(
         [Parameter(Mandatory=$True, Position=0, ValueFromPipeline=$True)]
             [string]$Text
         ,[Parameter(Mandatory=$False, Position=1, ValueFromPipeline=$False)]
-            [string]$Voice="Microsoft Zira Desktop"
+            [string]$Voice="Zira"
         ,[Parameter(Mandatory=$False, Position=2, ValueFromPipeline=$False)]
             [int]$Rate=2
         ,[Parameter(Mandatory=$False, Position=3, ValueFromPipeline=$False)]
@@ -15,9 +29,8 @@ function Out-TextToSpeech {
     )
     Add-Type -AssemblyName System.Speech
     $speechSynthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
-    $speechSynthesizer.SelectVoice($Voice)
+    $speechSynthesizer.SelectVoice("Microsoft $Voice Desktop")
     $speechSynthesizer.Rate = $Rate
     $speechSynthesizer.Volume = $Volume
     $speechSynthesizer.Speak($Text)
 }
-
