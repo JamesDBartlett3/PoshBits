ce9e565084fa2d4897594d84b2433d4235e0c29f
James D. Bartlett III <james.bartlett@gmail.com>
2023-02-09 21:00:20 -0600

    created Expand-CompressedFile

---
+++ Expand-CompressedFile.ps1
@@ -0,0 +1,32 @@
+#------------------------------------------------------------------------------------------------------------------
+# Title:      Expand-CompressedFile.ps1
+# Synopsis:   A PowerShell script that expands a compressed archive file
+# Author:     James Bartlett @jamesdbartlett3@techhub.social
+# Source:     https://github.com/jamesdbartlett3/PoshBits/blob/main/Expand-CompressedFile.ps1
+# 
+# Parameters: 
+#   1) $ArchiveFile = full path of the archive file (mandatory)
+#   2) $DestinationFolder = the full path of the destination folder (optional)
+# Requires:   PowerShell 5.1 or later
+#------------------------------------------------------------------------------------------------------------------
+
+param(
+  [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
+    [string]$ArchiveFile,
+  [Parameter(Mandatory=$false, Position=1, ValueFromPipeline=$true)]
+    [string]$DestinationFolder = $(Join-Path -Path $(Split-Path $ArchiveFile) -ChildPath (Split-Path $ArchiveFile -LeafBase))
+)
+
+# Check if the archive file exists
+if (!(Test-Path $ArchiveFile)) {
+  Write-Error "The archive file $ArchiveFile does not exist"
+  return
+}
+
+try {
+  Expand-Archive -Path $ArchiveFile -DestinationPath $DestinationFolder
+} catch {
+  Write-Error "Failed to expand the archive file $ArchiveFile"
+} finally {
+  Write-Host "The archive file $ArchiveFile was expanded to $DestinationFolder"
+}
