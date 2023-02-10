6f6dd3b93fec042b79d60895a3d09e6d67a2bdf3
James D. Bartlett III <james.bartlett@gmail.com>
2023-02-09 21:58:00 -0600

    added $Overwrite param

--- Expand-ZipFile.ps1
+++ Expand-ZipFile.ps1
@@ -7,6 +7,7 @@
 # Parameters: 
 #   1) $ZipFile = full path of the zip file (mandatory)
 #   2) $DestinationFolder = the full path of the destination folder (optional)
+#   3) $Overwrite = switch to overwrite any existing files (optional)
 # Requires:   PowerShell 5.1 or later
 #------------------------------------------------------------------------------------------------------------------
 
@@ -14,7 +15,9 @@ param(
   [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
     [string]$ZipFile,
   [Parameter(Mandatory=$false, Position=1, ValueFromPipeline=$true)]
-    [string]$DestinationFolder = $(Split-Path -LiteralPath $ZipFile)
+    [string]$DestinationFolder = $(Split-Path -LiteralPath $ZipFile),
+  [Parameter(Mandatory=$false, Position=2, ValueFromPipeline=$true)]
+    [switch]$Overwrite
 )
 
 $DestinationFolder = Join-Path -Path $DestinationFolder -ChildPath $(Split-Path -Path $ZipFile -LeafBase)
@@ -31,16 +34,16 @@ $contents = [string](Get-Content -Raw -Encoding Unknown -LiteralPath $ZipFile).T
 # If file is a zip file, attempt to expand it
 if ([convert]::tostring([convert]::toint32($contents[0]),16) -eq "4b50") {
   try {
-    Expand-Archive -LiteralPath $ZipFile -DestinationPath $DestinationFolder
+    # Expand the zip file. If $Overwrite is specified, overwrite any existing files
+    if ($Overwrite) {
+      Expand-Archive -LiteralPath $ZipFile -DestinationPath $DestinationFolder -Force
+    } else {
+      Expand-Archive -LiteralPath $ZipFile -DestinationPath $DestinationFolder
+    }
   } catch {
     # If the attempt fails, write the error to the console
     Write-Verbose $Error[0]
     Write-Error "Failed to expand the file $ZipFile"
-  } finally {
-    if(!$Error) {
-      # If it succeeds, write a success message to the console
-      Write-Host "The file $ZipFile was expanded to $DestinationFolder"
-    }
   }
 } else {
   # If the file is not a zip file, write a message to the console saying so
