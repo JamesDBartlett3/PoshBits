9fffa8bcf035fde6e34a137e2b26062c01be29af
James D. Bartlett III <james.bartlett@gmail.com>
2023-07-30 20:27:26 -0500

    completed script

--- Format-IndentSpacesAsTabs.ps1
+++ Format-IndentSpacesAsTabs.ps1
@@ -9,12 +9,64 @@
 		The file to format.
 	
 	.PARAMETER Indentation
-		The number of spaces to convert to tabs.
+		The number of spaces to convert to tabs. Default is 2.
 	
 	.EXAMPLE
-		PS C:\> Format-IndentSpacesAsTabs -InputFile ".\MyScript.ps1" -Indentation 4
+		PS C:\> Format-IndentSpacesAsTabs -InputFile ".\MyScript.ps1"
 	
 	.EXAMPLE
 		PS C:\> Get-ChildItem -Path ".\*.ps1" | Format-IndentSpacesAsTabs -Indentation 4
 	
 #>
+
+# Parameters
+Param(
+	
+	[Parameter(
+		Mandatory = $true,
+		ValueFromPipeline = $true
+	)]
+	[Alias("FullName")]
+	[ValidateNotNullOrEmpty()]
+	[string[]]$InputFile,
+	
+	[Parameter(
+		Mandatory = $false
+	)]
+	[ValidateNotNullOrEmpty()]
+	[int]$Indentation = 2
+	
+)
+
+# Process
+Begin {
+	
+	# Set the tab character
+	$tab = "`t"
+	
+	# Set the regex pattern
+	$pattern = " " * $Indentation
+	
+}
+
+Process {
+	
+	# Loop through each file
+	ForEach ($file in $InputFile) {
+		
+		# Read the file
+		$content = Get-Content -LiteralPath $file
+		
+		# Replace the spaces with tabs
+		$content.Replace($pattern, $tab) | Set-Content -LiteralPath $file -Force
+		
+	}
+	
+}
+
+End {
+	
+	# Clear the variables
+	Remove-Variable -Name tabWidth, tab, pattern -ErrorAction SilentlyContinue
+	
+}
