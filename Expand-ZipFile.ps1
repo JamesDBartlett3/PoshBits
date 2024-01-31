8c15e6e1898f1b39e3d360334e20e1217235d1fd
JamesDBartlett3 <37491308+JamesDBartlett3@users.noreply.github.com>
2024-01-31 17:27:50 -0600

    Refactor Expand-ZipFile.ps1 script for pipeline support

--- Expand-ZipFile.ps1
+++ Expand-ZipFile.ps1
@@ -1,54 +1,93 @@
-#------------------------------------------------------------------------------------------------------------------
-# Title:		 Expand-ZipFile.ps1
-# Synopsis:	 A PowerShell script that extracts a zip file to a specified folder
-# Author:		 James Bartlett @jamesdbartlett3@techhub.social
-# Source:		 https://github.com/jamesdbartlett3/PoshBits/blob/main/Expand-ZipFile.ps1
-# 
-# Parameters: 
-#	 1) $ZipFile = full path of the zip file (mandatory)
-#	 2) $DestinationFolder = the full path of the destination folder (optional)
-#	 3) $Overwrite = switch to overwrite any existing files (optional)
-# Requires:	 PowerShell 5.1 or later
-#------------------------------------------------------------------------------------------------------------------
+<#
+.SYNOPSIS
+	Extracts a zip file to a specified folder
+.DESCRIPTION
+	- Extracts a zip file to a specified folder.
+	- If the destination folder is specified, but does not exist, it will be created.
+	- If the destination folder is not specified, a new folder will be created in the same location as the zip file, 
+		with the same name as the zip file (minus the .zip extension), and the files will be extracted to that folder.
+	- If the $Overwrite switch is specified, any existing files and folders will be overwritten.
+	- If $ZipFile is not a valid zip file, an error will be written to the console.
+.PARAMETER ZipFile (Mandatory)
+	The path of the zip file to be extracted.
+.PARAMETER DestinationFolder (Optional)
+	The path of the folder to which the zip file will be extracted. If not specified, a new folder will be created in the same location as the zip file, with the same name as the zip file (minus the .zip extension).
+.PARAMETER Overwrite (Optional)
+	Specifies that any existing files and folders will be overwritten.
+.EXAMPLE
+	# Extract the contents of the zip file "C:\Temp\MyZipFile.zip" to the folder "C:\Temp\MyFolder", overwriting any existing files and folders.
+	.\Expand-ZipFile.ps1 -ZipFile "C:\Temp\MyZipFile.zip" -DestinationFolder "C:\Temp\MyFolder" -Overwrite
+.EXAMPLE
+	# Take output from Get-ChildItem and extract the contents of each zip file into a new folder with the same name (minus the .zip extension) and parent directory as that zip file, skipping any existing files and folders.
+	Get-ChildItem -Recurse -Path "C:\Temp" -Filter "*.zip" | .\Expand-ZipFile.ps1
+.NOTES
+	Requires PowerShell 5.1 or later.
+.LINK
+	[Original source code](https://github.com/jamesdbartlett3/PoshBits/blob/main/Expand-ZipFile.ps1)
+.LINK
+	[Read the author's blog](https://datavolume.xyz)
+.LINK
+	[Follow the author on Mastodon](https://techhub.social/@jamesdbartlett3)
+.LINK
+	[Follow the author on Bluesky](https://bsky.app/profile/jamesdbartlett3.bsky.social)
+.LINK 
+	[Follow the author on GitHub](https://github.com/jamesdbartlett3)
+.LINK
+	[Follow the author on LinkedIn](https://www.linkedin.com/in/jamesdbartlett3)
+#>
 
 param(
-	[Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
-	[string]$ZipFile,
-	[Parameter(Mandatory = $false, Position = 1, ValueFromPipeline = $true)]
-	[string]$DestinationFolder = $(Split-Path -LiteralPath $ZipFile),
-	[Parameter(Mandatory = $false, Position = 2, ValueFromPipeline = $true)]
-	[switch]$Overwrite
+	[Parameter(Mandatory, ValueFromPipelineByPropertyName)][Alias("FullName")][string]$ZipFile
+	, [Parameter()][string]$DestinationFolder
+	, [Parameter()][switch]$Overwrite
 )
-
-$DestinationFolder = Join-Path -Path $DestinationFolder -ChildPath $(Split-Path -Path $ZipFile -LeafBase)
-
-# Check if the file exists
-if (!(Test-Path -LiteralPath $ZipFile)) {
-	Write-Error "The file $ZipFile does not exist"
-	return
+begin {
+	Write-Verbose "Expanding Zip file(s)..."
 }
-
-# Check the file header to see if it is a zip file
-$contents = [string](Get-Content -Raw -Encoding Unknown -LiteralPath $ZipFile).ToCharArray()
-
-# If file is a zip file, attempt to expand it
-if ([convert]::tostring([convert]::toint32($contents[0]), 16) -eq "4b50") {
-	try {
-		# Expand the zip file. If $Overwrite is specified, overwrite any existing files
-		if ($Overwrite) {
-			Expand-Archive -LiteralPath $ZipFile -DestinationPath $DestinationFolder -Force
+process {
+	if (!($DestinationFolder)) {
+		$DestinationFolder = [System.IO.Path]::Combine((Get-Item -LiteralPath $ZipFile).DirectoryName, [System.IO.Path]::GetFileNameWithoutExtension($ZipFile))
+	}
+	# Check if the file exists
+	if (!(Test-Path -LiteralPath $ZipFile)) {
+		Write-Error "The file $ZipFile does not exist."
+		return
+	}
+	# Check if the destination folder exists. If not, create it
+	if (!(Test-Path -LiteralPath $DestinationFolder)) {
+		try {
+			New-Item -Path $DestinationFolder -ItemType Directory -ErrorAction Stop | Out-Null
 		}
-		else {
-			Expand-Archive -LiteralPath $ZipFile -DestinationPath $DestinationFolder
+		catch {
+			Write-Error "Failed to create the folder $DestinationFolder."
+			return
 		}
 	}
- catch {
-		# If the attempt fails, write the error to the console
-		Write-Verbose $Error[0]
-		Write-Error "Failed to expand the file $ZipFile"
+	# Check the file header to see if it is a zip file
+	$contents = [string](Get-Content -Raw -Encoding Unknown -LiteralPath $ZipFile).ToCharArray()
+	# Escape illegal characters in the file paths
+	$DestinationFolder = [Management.Automation.WildcardPattern]::Escape($DestinationFolder)
+	$ZipFile = [Management.Automation.WildcardPattern]::Escape($ZipFile)
+	# If file is a zip file, attempt to expand it
+	if ([convert]::tostring([convert]::toint32($contents[0]), 16) -eq "4b50") {
+		try {
+			# Create expression to expand the zip file. If $Overwrite switch is specified, add the -Force switch to the Expand-Archive cmdlet
+			[string]$Expression = "Expand-Archive -Path '$ZipFile' -DestinationPath '$DestinationFolder' $(if ($Overwrite) { '-Force' })"
+			# Invoke the expression
+			Write-Host "Invoking expression: $Expression"
+			Invoke-Expression $Expression
+		}
+		catch {
+			# If the attempt fails, write the error to the console
+			Write-Verbose $Error[0]
+			Write-Error "Failed to expand the file $ZipFile."
+		}
 	}
-}
-else {
-	# If the file is not a zip file, write a message to the console saying so
-	Write-Error "The file $ZipFile is not a zip file"
+	else {
+		# If the file is not a zip file, write a message to the console saying so
+		Write-Error "The file $ZipFile is not a zip file."
+	}
+	# Clear the variables
+	$DestinationFolder = $null
+	$ZipFile = $null
 }
